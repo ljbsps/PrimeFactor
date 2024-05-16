e1292aa12b9d013549c67cb2346c6bc98af72c4f
jb0114.lee <jb0114.lee@samsung.com>
2024-05-16 16:39:04 +0900

    [feature] page 25

--- Sample-Test1/prime-factors_test.cpp
+++ Sample-Test1/prime-factors_test.cpp
@@ -1,7 +1,9 @@
 #include "pch.h"
 #include "../PrimeFactor/prime-factors.cpp"
 
-TEST(TestCaseName, TestName) {
+TEST(PrimeFactors, PrimeTest) {
   EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
+  EXPECT_EQ(1, 1);
 }
